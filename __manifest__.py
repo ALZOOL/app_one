abdf4f7c144e05a9d97961cf8a013ea5a95b16b2
ALZOOL <aymanjamma03@gmail.com>
2025-12-27 13:44:14 +0200

    Initial app_one module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':"App One",
+    'author':'Ayman Jammaa',
+    'category':'',
+    'version':'18.0.0.1',
+    'depends':['base',
+              ],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/property_view.xml',
+        
+    ],
+    'application': True,
+
+}
---
+++ models/model_c.py
@@ -0,0 +1,4 @@
+from odoo import models
+
+class ModelC(models.Model):
+    _name = 'model.c'
